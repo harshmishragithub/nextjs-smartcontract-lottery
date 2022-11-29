0005d180776a168f2a533d1ef8f9fc42f8acd756
harsh <harshmishra9013@gmail.com>
2022-11-29 10:44:12 +0530

    commit

--- components/LotteryEntrance.js
+++ components/LotteryEntrance.js
@@ -1,13 +1,11 @@
 import { useWeb3Contract } from "react-moralis";
-import { contractAddress, abi } from "../constants";
-
+import { abi, contractAddress } from "../constants";
 import { useMoralis } from "react-moralis";
 import { useEffect, useState } from "react";
 import { ethers } from "ethers";
 import { useNotification } from "web3uikit";
-
 export default function LotteryEntrance() {
-  const { chainId: chainIdHex, isWeb3Enabled } = useMoralis();
+  const { chainId: chainIdHex, isWeb3Enabled, web3 } = useMoralis();
   const chainId = parseInt(chainIdHex);
   const raffleAddress =
     chainId in contractAddress ? contractAddress[chainId][0] : null;
@@ -31,28 +29,28 @@ export default function LotteryEntrance() {
 
   const { runContractFunction: getEntranceFee } = useWeb3Contract({
     abi: abi,
-    contractAddress: raffleAddress,
+    contractAddress: raffleAddress, // specify the networkId
     functionName: "getEntranceFee",
     params: {},
   });
 
   const { runContractFunction: getNumberOfPlayers } = useWeb3Contract({
     abi: abi,
-    contractAddress: raffleAddress,
+    contractAddress: raffleAddress, // specify the networkId
     functionName: "getNumberOfPlayers",
     params: {},
   });
 
   const { runContractFunction: getRecentWinner } = useWeb3Contract({
     abi: abi,
-    contractAddress: raffleAddress,
+    contractAddress: raffleAddress, // specify the networkId
     functionName: "getRecentWinner",
     params: {},
   });
 
   async function updateUI() {
-    const entranceFeeFromCall = await getEntranceFee();
-    const numPlayersFromCall = await getNumberOfPlayers();
+    const entranceFeeFromCall = (await getEntranceFee()).toString();
+    const numPlayersFromCall = (await getNumberOfPlayers()).toString();
     const recentWinnerFromCall = await getRecentWinner();
     setEntranceFee(entranceFeeFromCall);
     setNumPlayers(numPlayersFromCall);
@@ -62,57 +60,69 @@ export default function LotteryEntrance() {
   useEffect(() => {
     if (isWeb3Enabled) {
       updateUI();
+      listenForWinnerToBePicked();
     }
   }, [isWeb3Enabled]);
 
   const handleSuccess = async function (tx) {
     await tx.wait(1);
     handleNewNotification(tx);
-
     updateUI();
   };
-  const handleNewNotification = function () {
+
+  async function listenForWinnerToBePicked() {
+    const filter = {
+      address: raffleAddress,
+      topics: [ethers.utils.id("WinnerPicked(address)")],
+    };
+
+    web3.on(filter, (result) => {
+      console.log("Winner Picked");
+      updateUI();
+    });
+  }
+
+  const handleNewNotification = () => {
     dispatch({
       type: "info",
-      message: "Transaction Complete",
-      title: "Tx Notification",
+      message: "Transaction Complete!",
+      title: "Transaction Notification",
       position: "topR",
-      icon: "bell",
     });
   };
 
   return (
-    <div className="p-5">
-      <h1 className="py-4 px-4 font-bold text-3xl">Lottery</h1>
+    <div>
       {raffleAddress ? (
-        <>
+        <div>
           <button
+            disabled={isLoading || isFetching}
             className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded ml-auto"
-            onClick={async () =>
+            onClick={async function () {
               await enterRaffle({
-                // onComplete:
-                // onError:
                 onSuccess: handleSuccess,
                 onError: (error) => console.log(error),
-              })
-            }
-            disabled={isLoading || isFetching}
+              });
+            }}
           >
             {isLoading || isFetching ? (
               <div className="animate-spin spinner-border h-8 w-8 border-b-2 rounded-full"></div>
             ) : (
-              "Enter Raffle"
+              <div>Enter Raffle</div>
             )}
           </button>
           <div>
             Entrance Fee: {ethers.utils.formatUnits(entranceFee, "ether")} ETH
           </div>
-          <div>The current number of players is: {numberOfPlayers}</div>
-          <div>The most previous winner was: {recentWinner}</div>
-        </>
+          <div>Number Of Players: {numPlayers}</div>
+          <div>Recent Winner : {recentWinner}</div>
+        </div>
       ) : (
-        <div>Please connect to a supported chain </div>
+        <div>No Raffle Address Detected</div>
       )}
     </div>
   );
 }
+
+//0xB20765a111A03410A4a8DF50cd8293410666a37a
+// LOCAL -> 0xCf7Ed3AccA5a467e9e704C703E8D87F634fB0Fc9 || 31337
